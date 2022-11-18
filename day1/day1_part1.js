a08e89bed6516737a210b675093b6217b7e6a0e8
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-11-18 16:12:55 +0100

    update

--- day1/day1_part1.js
+++ day1/day1_part1.js
@@ -7,7 +7,7 @@ function day1(input) {
         const contents = fs.readFileSync(input, 'utf-8');
         const arr = contents.split(/\r?\n/);
         console.log(arr);
-        return arr.reduce(function(p,c,i,a) {c < a[i+1] ? p++ : p; return p;},0) + 1;
+        return arr.reduce(function(p,c,i,a) {c < a[i+1] ? p++ : p; return p;},1);
     } catch (error) {
         console.log(error);
     }
