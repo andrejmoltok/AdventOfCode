4e9aedf43ca9d920f214d45e8f1d48e0ed61ebaf
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-11-18 16:02:55 +0100

    second attempt

--- day1/day1_part2.js
+++ day1/day1_part2.js
@@ -7,21 +7,21 @@ function day1(input) {
         const contents = fs.readFileSync(input, 'utf-8');
         const arr = contents.split(/\r?\n/);
         // console.log(arr);
-        let count = 0;
+        let count = -1;
         let prev = 0;
-        for (let i = 0; i < arr.length-1; i++) {
+        for (let i = 0; i < arr.length-2; i++) {
             let curr = 0;
             for (let j = 0; j < 3; j++) {
                 curr += Number(arr[i+j]);
-                console.log(i,j,arr[i+j]);
+                //console.log(i,j,arr[i+j]);
             }
             if (prev < curr) {
                 prev = curr;
                 count++;  
             }
-            
-            console.log(prev, curr);
+            //console.log(prev, curr,count);
         }
+        console.log(arr.length);
         return count;
     } catch (error) {
         console.log(error);
