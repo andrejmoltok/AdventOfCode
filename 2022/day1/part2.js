495b22ee11b59a0f4f50cad2f2a45f4f349da824
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-12-09 11:39:39 +0100

    part1 SOLVED

--- 2022/day1/part2.js
+++ 2022/day1/part2.js
@@ -1,32 +1,28 @@
 'use strict'
 var fs = require('fs');
 
-function part1(input) {
+function part2(input) {
     try {
         const content = fs.readFileSync(input, 'utf-8');
         const arr = content.split('\r\n');
-        //console.log(arr);
         let max = 0;
         let localSum = 0;
         let final = 0;
         for (let i = 0; i < arr.length; i++) {
-            var mySum = 0;
             localSum += +arr[i];
             if (arr[i] === '') {
-                mySum = Math.max(max,localSum);
+                final = Math.max(max,localSum);
                 localSum = 0;
             }
-            if (mySum > 0) {
-                max = mySum;
+            if (final > max) {
+                max = final;
             }
             final = Math.max(max,localSum)
-            //console.log(localSum, mySum);
         }
-       
         return final;
     } catch (error) {
         console.log(error);
     }
 }
 
-console.log(part1('input.txt'));
+console.log(part2('input2.txt'));
